7ed1d0911cb34bb701b325f86d41179bdc416fc1
Ricardo Herrera <rickhl@outlook.com>
2021-11-14 13:23:04 -0500

    Make avatar part of the header link

--- src/components/header.js
+++ src/components/header.js
@@ -16,13 +16,22 @@ const Header = () => {
     `)
 
     return(
-      <header css={css(tw`flex py-5 gap-2 md:py-8 lg:py-10 lg:gap-3`)}>
+      <header css={css(tw`py-5 md:py-8 lg:py-10`)}>
+          <Link css={css(`
+            :hover {
+              color: red;
+            }
+          `,
+            tw`text-sm md:text-base lg:text-lg text-current`
+          )} to="/">
           <StaticImage css={css`
               border-radius: 50%;
               overflow: hidden;
               perspective: 0px;
               width: 60px;
+              margin-right: 0.5rem;
               height: 60px;
+              vertical-align: middle;
               border: 2px black solid;
               @media (max-width: 1024px) {
                 width: 50px;
@@ -36,13 +45,7 @@ const Header = () => {
             }
             src="../images/avatar.png"
           />
-          <Link css={css(`
-            :hover {
-              color: red;
-            }
-          `,
-            tw`text-sm md:text-base lg:text-lg text-current self-center`
-          )} to="/">{data.site.siteMetadata.title}</Link>
+            {data.site.siteMetadata.title}</Link>
       </header>
     )
   }

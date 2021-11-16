1dbb64516054fbd08701b27f3e60205bc182e295
Ricardo Herrera <rickhl@outlook.com>
2021-11-16 11:01:31 -0500

    :rotating_light: Fix linter warnings and errors

--- src/components/header.js
+++ src/components/header.js
@@ -1,11 +1,11 @@
-import * as React from 'react'
-import { css } from "@emotion/react"
-import { Link, useStaticQuery, graphql } from "gatsby"
-import tw from 'twin.macro'
-import { StaticImage } from 'gatsby-plugin-image'
+import * as React from 'react';
+import { css } from '@emotion/react';
+import { Link, useStaticQuery, graphql } from 'gatsby';
+import tw from 'twin.macro';
+import { StaticImage } from 'gatsby-plugin-image';
 
 const Header = () => {
-    const data = useStaticQuery(graphql`
+  const data = useStaticQuery(graphql`
       query {
         site {
           siteMetadata {
@@ -13,18 +13,24 @@ const Header = () => {
           }
         }
       }
-    `)
+    `);
 
-    return(
-      <header css={css(tw`py-5 md:py-7 lg:py-9`)}>
-          <Link css={css(`
+  return (
+    <header css={css(tw`py-5 md:py-7 lg:py-9`)}>
+      <Link
+        css={css(
+          `
             :hover {
               color: red;
             }
           `,
-            tw`text-sm text-current lg:text-lg`
-          )} to="/">
-          <StaticImage loading="eager" css={css`
+          tw`text-sm text-current lg:text-lg`,
+        )}
+        to="/"
+      >
+        <StaticImage
+          loading="eager"
+          css={css`
               border-radius: 50%;
               overflow: hidden;
               perspective: 0px;
@@ -41,14 +47,13 @@ const Header = () => {
                 width: 50px;
                 height: 50px;
               } 
-            }`
-            }
-            src="../images/avatar.png"
-          />
-          <span css={css(tw`hidden md:inline-flex`)}>{data.site.siteMetadata.title}</span>
-            </Link>
-      </header>
-    )
-  }
+            }`}
+          src="../images/avatar.png"
+        />
+        <span css={css(tw`hidden md:inline-flex`)}>{data.site.siteMetadata.title}</span>
+      </Link>
+    </header>
+  );
+};
 
-export default Header
+export default Header;

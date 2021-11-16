fe54d2b5e9248d68ac38ad632d8e5ad8c5d19d7c
Ricardo Herrera <rickhl@outlook.com>
2021-11-16 13:53:32 -0500

    :rotating_light: Fix prettier warnings and errors

--- src/components/social.js
+++ src/components/social.js
@@ -1,5 +1,5 @@
-import * as React from 'react';
-import { Link } from 'gatsby';
+import * as React from "react";
+import { Link } from "gatsby";
 
 export const Instagram = () => (
   <Link to="https://instagram.com/rickhlx">

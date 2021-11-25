6558523e5b030fcdd41142a3574bfcb5532285d4
Ricardo Herrera <rickhl@outlook.com>
2021-11-25 17:56:30 -0500

    :sparkles: Enable dark mode on website

---
+++ src/components/darkmodetoggle.js
@@ -0,0 +1,43 @@
+import React from "react";
+import useDarkMode from "use-dark-mode";
+import styled from "@emotion/styled";
+import { css } from "@emotion/react";
+import { FiMoon, FiSun } from "react-icons/fi";
+import tw from "twin.macro";
+
+const DarkModeIcon = styled(FiMoon)({
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+  margin: "0",
+});
+
+const DefaultModeIcon = styled(FiSun)({
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+  margin: "0",
+});
+
+const DarkModeToggle = () => {
+  const darkMode = useDarkMode();
+
+  return (
+    <button
+      css={css(
+        {
+          background: darkMode.value ? "#262F41" : "#EBEEF3",
+          transition: "all 0.3 ease",
+        },
+        tw`flex border-0 rounded-full w-10 h-10 p-0 m-0 
+        items-center justify-center text-current`
+      )}
+      type="button"
+      onClick={darkMode.toggle}
+    >
+      {darkMode.value ? <DefaultModeIcon /> : <DarkModeIcon />}
+    </button>
+  );
+};
+
+export default DarkModeToggle;

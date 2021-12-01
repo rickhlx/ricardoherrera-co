838c6d283f091bceafae0e932b1f6ddd87274837
Ricardo Herrera <rickhl@outlook.com>
2021-12-01 09:11:04 -0500

    Add meta to SEO and allow title to change

--- src/components/seo.js
+++ src/components/seo.js
@@ -2,18 +2,56 @@ import * as React from "react";
 import { useStaticQuery, graphql } from "gatsby";
 import { Helmet } from "react-helmet";
 
-const SEO = () => {
+const SEO = ({ title, description, template }) => {
   const data = useStaticQuery(graphql`
     query {
       site {
         siteMetadata {
           title
+          author
+          description
+          keywords
+          siteUrl
         }
       }
     }
   `);
 
-  return <Helmet title={data.site.siteMetadata.title} />;
+  const metaDescription = description || data.site.siteMetadata.description;
+
+  return (
+    <Helmet
+      title={title ? title.toLowerCase() : ""}
+      titleTemplate={template ? `%s | ${data.site.siteMetadata.title}` : ""}
+      defaultTitle={data.site.siteMetadata.title}
+      meta={[
+        {
+          name: "description",
+          content: metaDescription,
+        },
+        {
+          name: "keywords",
+          content: data.site.siteMetadata.keywords.join(","),
+        },
+        {
+          property: `og:title`,
+          content: title,
+        },
+        {
+          property: `og:description`,
+          content: metaDescription,
+        },
+        {
+          property: `og:type`,
+          content: `website`,
+        },
+      ]}
+    />
+  );
+};
+
+SEO.defaultProps = {
+  template: true,
 };
 
 export default SEO;
